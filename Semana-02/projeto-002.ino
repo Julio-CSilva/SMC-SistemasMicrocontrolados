331121dd56d7fd7024694a31bcfde6ccfcf6f1f9
Júlio Silva <juliocesardasilvafilho@hotmail.com>
2021-07-02 19:16:21 -0300

    Create projeto-002.ino
    Semana 02 - Projeto 02
    Acionamento de LEDs em sequência, 2s Verde, 1s Amarelo, 3s Vermelho, onde caso o botão esteja pressionado o sistema entende como solicitação de parada para passagem de pedestres, acionando o feitor sonoro do buzzer, por fim o sistema é reiniciado começando novamente o processo.

---
+++ Semana-02/projeto-002.ino
@@ -0,0 +1,43 @@
+int botao = 12;
+int ledVermelho = 11;
+int ledAmarelo = 10;
+int ledVerde = 9;
+int buzzer = 8;
+boolean parada = false;
+
+void setup() {
+  pinMode(botao, INPUT);
+  pinMode(ledVermelho, OUTPUT);
+  pinMode(ledAmarelo, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledVerde, HIGH);
+  delay(2000);
+  digitalWrite(ledVerde, LOW);
+  digitalWrite(ledAmarelo, HIGH);
+  delay(1000);
+  digitalWrite(ledAmarelo, LOW);
+  digitalWrite(ledVermelho, HIGH);
+  delay(3000);
+  digitalWrite(ledVermelho, LOW);
+
+  if(digitalRead(botao) == HIGH){
+    delay(10);
+    if(digitalRead(botao) == HIGH){
+      parada = true;
+    }
+  }
+
+  if(parada){
+    parada = false;
+    digitalWrite(ledVermelho, LOW);
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledAmarelo, LOW);
+    digitalWrite(buzzer, HIGH);
+    delay(2000);
+    digitalWrite(buzzer, LOW);
+  }
+}

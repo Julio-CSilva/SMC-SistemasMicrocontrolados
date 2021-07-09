f4ef09146d32659325bc549b6204a7b54dcd4722
Júlio Silva <juliocesardasilvafilho@hotmail.com>
2021-07-09 18:23:39 -0300

    Create projeto-002.ino
    Semana 03 - Projeto 02 - PIC18F45K20
    Configuração das portas e botão, deixando os LEDs DS0 = 0 e DS1 = 1, e ao pressionar o botão inverte-se os LEDs.

---
+++ Semana-03/projeto-002.ino
@@ -0,0 +1,38 @@
+/*
+ * File:   main.c
+ * Author: julio
+ *
+ * Created on 9 de Julho de 2021, 15:33
+ */
+
+
+#include <xc.h> //ARQUIVO READER QUE CONTEM AS CONFIGURAÇÕES DO uC escolhido
+
+#pragma config FOSC = INTIO67 //FONTE DE CLOCK
+#pragma config BOREN = OFF //CASO AJA QUEDA DE ENERGIA ELE REINICIA O uC
+
+#pragma config PBADEN = OFF //Comando necessario para poder utilizar as PORTB
+//DESLIGANDO O CONVERSOR ANALOGICO/DIGITAL das portas PORTB
+
+#define _XTAL_FREQ 1000000 //define a velocidade de repetição, *IGUAL CLOCK
+
+void main(void) {
+    
+    OSCCONbits.IRCF = 0b011; //Configurando o oscilador para TRABALHA com 1MHz
+    
+    TRISD = 0; //Configurar todos os pinos da PORTD - PD0 até PD7 como saída
+    
+    TRISBbits.RB0 = 1; //Config. o pino RB0 para ser entrada (BOTAO SW1)
+    
+    while(1){
+        if(PORTBbits.RB0 == 0){
+            PORTDbits.RD0 = 1;
+            PORTDbits.RD1 = 0;
+        } else {
+            PORTDbits.RD0 = 0;
+            PORTDbits.RD1 = 1;
+        }
+    }
+    
+    return;
+}

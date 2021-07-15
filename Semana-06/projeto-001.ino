65e4f4e23686286b430f1f8cbe8aa8334a724491
Júlio Silva <juliocesardasilvafilho@hotmail.com>
2021-07-15 15:13:42 -0300

    Update projeto-001.ino
    Semana 06 - Projeto 01 - PIC18F45K20
    Minigame, onde o jogador deve pressionar o botão no momento exato que os LEDS DS4 e DS5 estão acesos (00011000), ao acertar o momento exato da sequência, então todos os LEDs irão piscar por 2s.

--- Semana-06/projeto-001.ino
+++ Semana-06/projeto-001.ino
@@ -5,6 +5,23 @@
 
 #define _XTAL_FREQ 1000000 //SETTANDO FREQUENCIA 1MHz
 
+void __interrupt() bingo(){ //ACIONAMENTO DA INTERRUPÇÃO
+    if(INTCONbits.INT0IF == 1){ //DETECÇÃO DE INTERRUPÇÃO
+        INTCONbits.INT0IF = 0; //LIMPEZA DA FLAG
+        
+        if(PORTD == 0b00011000){ //CASO O ERRO ACONTEÇA NO MOMENTO DA SEQ 4
+            for(int i=0; i<10; i++){ //10X CICLO DE SUBIDA(100) E DESCIDA (100))
+                PORTD = 0b11111111;
+                __delay_ms(100);
+                PORTD = 0b00000000;
+                __delay_ms(100);
+            }
+        }
+        
+        __delay_ms(150);
+    }
+}
+
 void main(void) {
     OSCCONbits.IRCF = 0b011; //SELECIONANDO A CHAVE DE 1MHz
     
@@ -40,17 +57,3 @@ void main(void) {
     return;
 }
 
-void __interrupt() bingo(){
-    if(INTCONbits.INT0IF == 1){
-        INTCONbits.INT0IF = 0;
-        
-        if(PORTD == 0b00011000){
-            for(int i=0; i<10; i++){
-                PORTD = 0b11111111;
-                __delay_ms(100);
-                PORTD = 0b00000000;
-                __delay_ms(100);
-            }
-        }
-    }
-}
